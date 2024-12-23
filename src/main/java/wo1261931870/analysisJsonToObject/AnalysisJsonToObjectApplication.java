f6eb2a68599de3f14ccecdf154be1fc92a05f836
junw <wo1261931780@gmail.com>
2024-12-23 22:46:06 +0800

    初始化

---
+++ src/main/java/wo1261931870/analysisJsonToObject/AnalysisJsonToObjectApplication.java
@@ -0,0 +1,13 @@
+package wo1261931870.analysisJsonToObject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalysisJsonToObjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalysisJsonToObjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wo1261931870/analysisJsonToObject/AnalysisJsonToObjectApplicationTests.java
@@ -0,0 +1,13 @@
+package wo1261931870.analysisJsonToObject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalysisJsonToObjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
